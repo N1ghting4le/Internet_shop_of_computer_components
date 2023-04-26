be911827fc355ad46f99d94d7d18100833e7e4c8
N1ghting4le <nikansoloveib@gmail.com>
2023-04-26 16:41:34 +0300

    version 1.0.1 dynamic imports added

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,29 +1,29 @@
 import AppHeader from '../appHeader/AppHeader';
-import MainPage from '../../pages/mainPage/MainPage';
-import SingleCategoryPage from '../../pages/singleCategoryPage/SingleCategoryPage';
-import SingleItemPage from '../../pages/singleItemPage/SingleItemPage';
-import CartPage from '../../pages/cartPage/CartPage';
 
+import { lazy } from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Suspense } from 'react';
 
 import './App.css';
 
+const MainPage = lazy(() => import('../../pages/mainPage/MainPage'));
+const SingleCategoryPage = lazy(() => import('../../pages/singleCategoryPage/SingleCategoryPage'));
+const SingleItemPage = lazy(() => import('../../pages/singleItemPage/SingleItemPage'));
+const CartPage = lazy(() => import('../../pages/cartPage/CartPage'));
+
 function App() {
   return (
-    <>
-      <Suspense>
-        <Router>
-          <AppHeader/>
-          <Routes>
-            <Route path='/' element={<MainPage/>}/>
-            <Route path='/:category' element={<SingleCategoryPage/>}/>
-            <Route path='/:category/:id' element={<SingleItemPage/>}/>
-            <Route path='/cart' element={<CartPage/>}/>
-          </Routes>
-        </Router>
-      </Suspense>
-    </>
+    <Suspense>
+      <Router>
+        <AppHeader/>
+        <Routes>
+          <Route path='/' element={<MainPage/>}/>
+          <Route path='/:category' element={<SingleCategoryPage/>}/>
+          <Route path='/:category/:id' element={<SingleItemPage/>}/>
+          <Route path='/cart' element={<CartPage/>}/>
+        </Routes>
+      </Router>
+    </Suspense>
   );
 }
 
